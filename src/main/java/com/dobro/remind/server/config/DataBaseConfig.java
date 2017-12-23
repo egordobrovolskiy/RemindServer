4f692c3f61115329496ddfd7880d651b62b8c610
egordobrovolskiy <31988202+egordobrovolskiy@users.noreply.github.com>
2017-12-23 22:30:34 +0200

    Add db.properties and DatabaseConfig.

---
+++ src/main/java/com/dobro/remind/server/config/DataBaseConfig.java
@@ -0,0 +1,22 @@
+package com.dobro.remind.server.config;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.annotation.Resource;
+
+@Configuration
+@EnableJpaRepositories("com.dobro.remind.server.repository")
+@EnableTransactionManagement
+@PropertySource("classpath:db.properties")
+@ComponentScan("com.dobro.remind.server")
+public class DataBaseConfig {
+
+    @Resource
+    private Environment env;
+}
