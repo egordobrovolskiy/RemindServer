edaead746ddf66a3ceed9de9b188805064a148cb
egordobrovolskiy <31988202+egordobrovolskiy@users.noreply.github.com>
2017-12-24 17:05:26 +0200

    Create Entity. Add properties for dbcp2.

--- src/main/java/com/dobro/remind/server/config/DataBaseConfig.java
+++ src/main/java/com/dobro/remind/server/config/DataBaseConfig.java
@@ -8,8 +8,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.annotation.Resource;
@@ -45,7 +47,23 @@ public class DataBaseConfig {
         dataSource.setDriverClassName(env.getRequiredProperty("driver"));
         dataSource.setUsername(env.getProperty("username"));
         dataSource.setPassword(env.getRequiredProperty("password"));
+
+        dataSource.setInitialSize(Integer.valueOf(env.getRequiredProperty("initialSize")));
+        dataSource.setMinIdle(Integer.valueOf(env.getRequiredProperty("minIdle")));
+        dataSource.setMaxIdle(Integer.valueOf(env.getRequiredProperty("maxIdle")));
+        dataSource.setTimeBetweenEvictionRunsMillis(Long.valueOf(env.getRequiredProperty("timeBetweenEvictionRunsMillis")));
+        dataSource.setMinEvictableIdleTimeMillis(Long.valueOf(env.getRequiredProperty("minEvictableIdleTimeMillis")));
+        dataSource.setTestOnBorrow(Boolean.valueOf(env.getRequiredProperty("testOnBorrow")));
+        dataSource.setValidationQuery(env.getRequiredProperty("validationQuery"));
         return dataSource;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        JpaTransactionManager manager = new JpaTransactionManager();
+        manager.setEntityManagerFactory(entityManagerFactory().getObject());
+
+        return manager;
     }
 
     private Properties getHibernateProperties() {
