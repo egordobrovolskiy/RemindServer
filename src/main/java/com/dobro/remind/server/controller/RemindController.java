5199ff40d16ef0dd7887ccfcad34af7f6b378582
Egor Dobrovolskiy <egordobrovolskiy@gmail.com>
2018-03-05 16:48:12 +0200

    Fixx

--- src/main/java/com/dobro/remind/server/controller/RemindController.java
+++ src/main/java/com/dobro/remind/server/controller/RemindController.java
@@ -4,11 +4,8 @@ package com.dobro.remind.server.controller;
 import com.dobro.remind.server.entity.Remind;
 import com.dobro.remind.server.service.RemindService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -31,7 +28,7 @@ public class RemindController {
 
     @RequestMapping(value = "/reminders", method = RequestMethod.POST)
     @ResponseBody
-    public Remind saveRemid(@RequestBody Remind remind) {
+    public Remind saveRemind(@RequestBody Remind remind) {
         return service.save(remind);
     }
 
